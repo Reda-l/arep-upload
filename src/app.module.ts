fc63c76b4ef04f98ab36859692b44626401681c8
Reda Laarissa <laarissareda@gmail.com>
2023-12-12 00:21:03 +0100

    upload img controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BunnycdnService } from './core/bunnycdn/bunnycdn.service';
+import { ProgramsModule } from './modules/programs/programs.module';
+import { ProjectsModule } from './modules/projects/projects.module';
 
 @Module({
-  imports: [],
+  imports: [ProgramsModule, ProjectsModule],
   controllers: [AppController],
   providers: [AppService, BunnycdnService],
 })
---
+++ src/modules/programs/programs.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { ProgramsService } from './programs.service';
+import { BunnycdnService } from 'src/core/bunnycdn/bunnycdn.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+
+@Controller('programs')
+export class ProgramsController {
+  constructor(private readonly programsService: ProgramsService, private bunnyCDNService: BunnycdnService) { }
+
+  // Upload img in bunnyCDN
+  @Post('upload/image')
+  @UseInterceptors(
+    FileInterceptor('image')
+  )
+  async upload(@UploadedFile() file): Promise<string> {
+    let filePath: string = ''
+
+    const folderPath = 'programme/images'; // upload path in bunny
+
+    if (file) {
+      const { buffer } = file;
+      const currentDate = new Date().toISOString().replace(/[^a-zA-Z0-9]/g, '_').split('T')[0];
+      const currentTime = new Date().toISOString().split('T')[1].replace(/\..+/, '').replace(/:/g, '');
+      const originalName = file.originalname.replace(/[^a-zA-Z0-9.]/g, '_').replace(/ /g, '_');
+      const randomName = `${currentDate}_${currentTime}_${originalName}`;
+      filePath = `${folderPath}/${randomName}`;
+      await this.bunnyCDNService.uploadFile(filePath, buffer);
+    }
+    return filePath;
+  }
+}
