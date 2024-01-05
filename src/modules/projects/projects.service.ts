ee1b4e2cbbfe6e78df072bbc817c51224129f7a6
TahaBenHammou <103946547+TahaBenHammou@users.noreply.github.com>
2024-01-05 16:23:33 +0100

    add docker

--- src/modules/projects/projects.service.ts
+++ src/modules/projects/projects.service.ts
@@ -68,7 +68,16 @@ export class ProjectsService {
 
     // Launch Puppeteer
     const browser = await puppeteer.launch({
-      args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu'],
+      args: [
+        "--disable-setuid-sandbox",
+        "--no-sandbox",
+        "--single-process",
+        "--no-zygote",
+      ],
+      executablePath:
+        process.env.NODE_ENV === "production"
+          ? process.env.PUPPETEER_EXECUTABLE_PATH
+          : puppeteer.executablePath(),
     });
     const page = await browser.newPage();
 
